c8a6902b798b3875832f7db3d105c4b88234ac3c
Ihor Bobulych <ihorbobuly@gmail.com>
2025-02-28 11:02:25 +0200

    init: palindrome starter

---
+++ count_palindromes.py
@@ -0,0 +1,4 @@
+# Do not change the function signature
+def count_palindromes(sentence: str) -> int:
+    # TODO: Implement this function
+    pass
---
+++ palindrome.py
@@ -0,0 +1,4 @@
+# Do not change the function signature
+def palindrome(word: str) -> bool:
+    # TODO: Implement this function
+    pass
